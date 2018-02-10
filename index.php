17e427d2a2d423468e3f0505938d0d0a3e35642f
RSX9998 <crafter96@yandex.ru>
2018-02-10 19:02:47 +0300

    1

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP TEST</title>
+</head>
+<body>
+<?php echo '<p align="right"><h1>Welcome!</h1></p>
+<?php echo '<p align="right"><h3>You are on official site of RSX</h3></p>
+<img src="Без имени-1.jpg" style="margin-left: 1700px;" />
+<?php echo '<p align="left"><h2>About Me!</h2></p>
+<?php echo '<p><h3><p>Hi, my name is Nikita!</p>
+
+<p align="left">I'm studying php, html and python!</p>
+
+<p align="left">Also I have a channel on YouTube</p>
+
+<p align="left">https://www.youtube.com/channel/UCvw0ARj0_A09GMHcNUjvDFw</p>
+
+<p align="left">Subscribe!</h3></p>
+
+<div id="logo" style="height: 970px;">
+	<img src="./Veselcraft_files/vc.png">
+</div>
+<div id="footer">
+
+    	
+	<br><br><br><a href="http://veselcraft.ru/#" onclick="report();">REPORT</a> | 
+	<a href="http://veselcraft.ru/index.php?lu=ru">Русский язык</a> | 
+	<a href="http://veselcraft.ru/m/">Mobile Version</a> |
+	<a href="http://veselcraft.ru/97/">Version For Old Browsers</a>
+	<div id="adddd"></div>
+</div>
+<div id="sonic">
+	
+</div>
+
+
+	<script type="text/javascript" src="./Veselcraft_files/jquery.js.загружено"></script>
+	<script type="text/javascript" src="./Veselcraft_files/jquery-ui.js.загружено"></script>
+	<script type="text/javascript" src="./Veselcraft_files/jquery.window.js.загружено"></script>
+<script type="text/javascript">
+	
+	var i;
+	i = document.documentElement.clientHeight.toString();
+document.getElementById('logo').style.height = i+"px";
+function report() {
+			$.window({
+				title: "Report Bug",
+				content: $("#bugreport").html(),
+				resizable: false,
+				maximizable: false,
+				checkBoundary: true
+			});
+		}
+	</script>
+<div id="bugreport" style="display: none;">
+			Name*: <input type="text" name="name"><br><br>
+		E-mail: <input type="email" name="email"><br><br>
+		Text: <textarea name="text" cols="50" rows="5">		</textarea>
+				<input type="submit" name="submit">
+	
+
+</div>
+
+
+</body>
+</html>
